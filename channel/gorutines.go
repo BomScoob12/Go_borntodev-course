bc5fa17345fd245fb916b228c72b38d25a249667
BomScoob12 <sorawitkraukham@gmail.com>
2024-08-30 15:11:12 +0700

    feat: add concurrent tasks to goroutines

--- channel/gorutines.go
+++ channel/gorutines.go
@@ -2,10 +2,18 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func form(textForm string) {
 	for i := 0; i < 100; i++ {
 		fmt.Println(textForm, " :", i)
 	}
+}
+
+func main() {
+	fmt.Println("This is a concerrent runing, switching to another task")
+	go form("task 1")
+	go form("task 2")
+	time.Sleep(1 * time.Second)
 }
