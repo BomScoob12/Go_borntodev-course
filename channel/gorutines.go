aab1f388d9b9a31d0ed08fab4c8c6fe43add9f50
BomScoob12 <sorawitkraukham@gmail.com>
2024-08-30 21:12:08 +0700

    refactor: adding reference in channel and change line of code

--- channel/gorutines.go
+++ channel/gorutines.go
@@ -14,6 +14,6 @@ func form(textForm string) {
 func main() {
 	fmt.Println("This is a concerrent runing, switching to another task")
 	go form("task 1")
-	go form("task 2")
+	form("task 2")
 	time.Sleep(1 * time.Second)
 }
